803e0a0b82068631157734b8f63fcf649c3f615b
Alba Mulero Otón <albamulero@MacBook-Pro-de-Alba.local>
2020-09-07 15:59:22 -0500

    front

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/login', (req, res, next) => {
+  res.render('login')
+})
+
+router.get('/sigup', (req, res, next) => {
+  res.render('sigup')
+})
+
+router.get('/routes', (req, res, next) => {
+  res.render('routes')
+})
+
+router.get('/data', (req, res, next) => {
+  res.render('data')
+})
+
+router.get('/conctat', (req, res, next) => {
+  res.render('conctat')
+})
 module.exports = router;

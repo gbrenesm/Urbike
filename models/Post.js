9df0dece1515fb516293177a78c62e99c2ca1bdb
Guadalupe Brenes <brenesmesa@gmail.com>
2020-09-10 10:32:57 -0500

    Barra de navegación fija, imagen por defaul en posts

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,10 @@ const postSchema = new Schema(
   {
     title: String,
     content: String,
-    photo: String, 
+    photo: {
+      type: String,
+      default: "https://res.cloudinary.com/dxncdwsau/image/upload/v1599751527/urbike-Rodada-CDMX.jpg.jpg",
+    },
     creatorId: {
       type: Schema.Types.ObjectId,
       ref: "User"
